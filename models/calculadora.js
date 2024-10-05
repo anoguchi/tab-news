3408e673fbaf6baf063f8d410de8892d2bfd0a91
Alberto Noguchi <alberto2611@gmail.com>
2024-10-04 23:31:39 -0300

    add: `/api/v1/status` endpoint

--- models/calculadora.js
+++
@@ -1,9 +0,0 @@
-function somar(inputNumber01, inputNumber02) {
-  const notNumbers = isNaN(inputNumber01) || isNaN(inputNumber02);
-  if (notNumbers) {
-    return "Erro";
-  }
-  return inputNumber01 + inputNumber02;
-}
-
-exports.somar = somar;
